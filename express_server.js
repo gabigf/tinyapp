596c3ffa893e5461b65f4dbacc1e55df7bbf0950
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-10 04:55:00 +0000

    Implemented post request for new URLs and a redirect to the corresponding website

--- express_server.js
+++ express_server.js
@@ -14,7 +14,6 @@ function generateRandomString() {
 	}
 	return randomString;
 }
-generateRandomString()
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -23,15 +22,11 @@ const urlDatabase = {
 }
 
 app.get('/', (req, res) => {
-	res.send('Hello');
+	res.redirect('/urls');
 });
 
 app.get('/urls.json', (req, res) => {
 	res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get('/urls', (req, res) => {
@@ -48,9 +43,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+	const longURL = req.body['longURL']
+	const shortURL = generateRandomString();
+	urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
